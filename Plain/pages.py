805542482bf52e792727c80a943a3b4817dd55f8
MSommer-HD <72254924+MSommer-HD@users.noreply.github.com>
2020-12-30 09:31:22 +0100

    Creating the new layout

--- Plain/pages.py
+++ Plain/pages.py
@@ -8,35 +8,11 @@ from .models import Constants
 #If you want to display something other than text (e.g. a variable) you need to use the function
 #vars_for_template and make it return a dictionary. The index of the dictionary can then be used to display it on the page with {{ index }}.
 # it is key that you indicate from which model you return a variable.
-class Welcome(Page):
-
-    def vars_for_template(self):
-        return {'pool': Constants.pool,
-                'players': Constants.players_per_group,
-                'factor': Constants.efficiency_factor,
-                'max': Constants.max,
-                }
 
 
 
-# For each Question and Answer pair i created a new page. You can decide if you want to show the page by the
-# id _displayed line.
-
-## a form field is something the participant can interact with. Since we defined it on a player level, we must specify it in the form_model section.
-
-
-class Test_Control(Page):
-    form_model = 'player'
-    form_fields = ['test_control']
-
-
-class Results_Control(Page):
-    def vars_for_template(self):
-        return {'test_control': self.player.test_control}
-
-
-
-
+class Grouping(WaitPage):
+    group_by_arrival_time = True
 
 class Take(Page):
     # a form field is something the participant can interact with. Here we will asked them to pick how much they want to take
@@ -48,8 +24,11 @@ class Take(Page):
 
 class ResultsWaitPage(WaitPage):
     # We use after_all_players_arrive to make sure we only start our calculation after every participant made their choice.
+    # self.group.set_payoff and set_breakdown runs the functions we defined on the models page. It is important that we run
+    # set_breakdown first, since it is an input for the payoff.
 
     def after_all_players_arrive(self):
+        self.group.set_breakdown()
         self.group.set_payoffs()
 
 
@@ -63,10 +42,14 @@ class Results(Page):
             take = self.player.take,
             total_points_left = self.group.total_points_left,
             pool_mult = self.group.total_points_left * Constants.efficiency_factor,
+            breakdown = self.group.breakdown,
+            treatment = self.subsession.treatment,
             share = self.group.resource_share,
             completion_code=self.player.completion_code
         )
 
 
-# here we indicate in which sequence we want the pages to the played. You can repeat pages as well.
-page_sequence = [Welcome, Test_Control, Results_Control, Take, ResultsWaitPage, Results]
+page_sequence = [Grouping,
+                 Take,
+                 ResultsWaitPage,
+                 Results]
