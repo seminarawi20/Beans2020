f4f2739c4899db95f4e472c5d0c12503844afec9
teymess <tillmann.eymess@awi.uni-heidelberg.de>
2022-01-12 12:49:02 +0100

    grouping fix for control

--- Endogen/pages.py
+++ Endogen/pages.py
@@ -117,7 +117,7 @@ class Results_Control2(Page):
     def before_next_page(self):
         self.participant.vars['wait_page_arrival'] = time.time()
         if self.timeout_happened:
-            self.player.timeout_test_result2 = True
+            self.player.timeout_ctest_result2 = True
 
 class Test1(Page):
     form_model = 'player'
