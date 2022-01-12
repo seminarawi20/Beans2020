a87b6a392b2ca727fe13b50a1d2353873fb71e7a
teymess <tillmann.eymess@awi.uni-heidelberg.de>
2022-01-12 11:08:59 +0100

    max earnings adjust

--- Endogen/models.py
+++ Endogen/models.py
@@ -40,7 +40,7 @@ class Constants(BaseConstants):
     completion_code = 112021 # Please change this number in your live version. This is just a random code all participants in the live version get
     #after they complete the experiment.
     base_payment = 0.5  # The amount of money (in $) the player gets just for participating
-    money_per_point = 0.1
+    money_per_point = 0.05
     maxmoney = int(np.floor(money_per_point*23.33+base_payment))
 
 
